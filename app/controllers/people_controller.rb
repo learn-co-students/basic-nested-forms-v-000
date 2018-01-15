367823024f616c1c3ec709098741407ff7a43f93
Robert Laws <roblaws@me.com>
2018-01-15 12:20:54 +0000

    Done.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,20 +1,36 @@
 class PeopleController < ApplicationController
-  def new
-    @person = Person.new
-  end
-
-  def create    
-    Person.create(person_params)
-    redirect_to people_path
-  end
-
   def index
     @people = Person.all
+  end
+
+  def show
+    @person = Person.find(params[:id])
+  end
+
+  def new
+    @person = Person.new
+    @person.addresses.build(address_type: "work")
+    @person.addresses.build(address_type: "home")
+  end
+
+  def create
+    Person.create(person_params)
+    redirect_to people_path
   end
 
   private
 
   def person_params
-    params.require(:person).permit(:name)
+    params.require(:person).permit(
+      :name,
+      address_attributes: [
+        :street_address_1,
+        :street_address_2,
+        :city,
+        :state,
+        :zipcode,
+        :address_type
+      ]
+    )
   end
 end
