cc6a4ed2453acaa7386294ad2447e10338989f8a
ElvinGarcia <dimix181@gmail.com>
2018-08-06 15:26:34 -0400

    walkthrough completed

--- app/models/person.rb
+++
@@ -1,4 +0,0 @@
-class Person < ActiveRecord::Base
-  has_many :addresses
-
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160114112845) do
+ActiveRecord::Schema.define(version: 20180806010321) do
 
   create_table "addresses", force: :cascade do |t|
     t.string   "street_address_1"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20160114112845) do
     t.string   "address_type"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
-    t.integer  "person_id"
+    t.integer  "people_id"
   end
 
   create_table "people", force: :cascade do |t|
