0d80af2a3b23df406d6784ce505decdec5cb5cc1
learn <albazaz@gmail.com>
2018-08-09 13:18:47 -0700

    Done.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,9 @@
 class Person < ActiveRecord::Base
   has_many :addresses
+  accepts_nested_attributes_for :addresses
 
+  def artist_attributes=(artist)
+    self.artist = Artist.find_or_create_by(name: artist.name)
+    self.artist.update(artist)
+  end
 end
