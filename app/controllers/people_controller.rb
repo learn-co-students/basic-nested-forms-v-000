afb3c421999300e1864ce52b185bbd183ab6f1fd
cjwood032 <cjwood032@gmail.com>
2018-08-12 09:57:19 -0400

    Done.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,6 +1,8 @@
 class PeopleController < ApplicationController
   def new
     @person = Person.new
+    @person.addresses.build(address_type: "work")
+    @person.addresses.build(address_type: "home")
   end
 
   def create    
@@ -15,6 +17,16 @@ class PeopleController < ApplicationController
   private
 
   def person_params
-    params.require(:person).permit(:name)
+    params.require(:person).permit(
+      :name,
+      addresses_attributes: [
+        :street_address_1,
+        :street_address_2,
+        :city,
+        :state,
+        :zipcode,
+        :address_type
+      ]
+    )
   end
 end
