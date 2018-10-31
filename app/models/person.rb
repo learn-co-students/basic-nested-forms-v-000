0dd947ba9d7f53d4d554ccba6fabb059b081919f
Dani Lyn <dlyn1110@gmail.com>
2018-10-31 19:42:09 +0000

    Done.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,18 @@
 class Person < ActiveRecord::Base
   has_many :addresses
+  accepts_nested_attributes_for :addresses
 
+  def person_params
+  params.require(:person).permit(
+    :name,
+    addresses_attributes: [
+      :street_address_1,
+      :street_address_2,
+      :city,
+      :state,
+      :zipcode,
+      :address_type
+    ]
+  )
+end
 end
