a61e22bb2a0d035a044fb10f699cc0e80be8f430
VincePimentel <contact@vincep.me>
2019-02-04 15:18:24 -0800

    Done.

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,2 +1,23 @@
 class AddressesController < ApplicationController
+
+  def new
+    @person = Person.new
+    @person.addresses.build(address_type: 'work')
+    @person.addresses.build(address_type: 'home')
+  end
+
+  def create
+    person = Person.create(person_params)
+    redirect_to people_path
+  end
+
+  def index
+    @people = Person.all
+  end
+
+  private
+
+  def person_params
+    params.require(:person).permit(:name)
+  end
 end
