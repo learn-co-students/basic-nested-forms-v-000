6b19f07c26cc975b8fa51419bf899e81bd524483
brennanmrose <brennan.m.rose@gmail.com>
2019-03-25 09:02:34 -0400

    Done.

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,11 +2,4 @@ class Person < ActiveRecord::Base
   has_many :addresses
   accepts_nested_attributes_for :addresses
 
-  # instead of accepts_nested_attributes_for we could write 
-  # 	def addresses_attributes=(addresses_attributes)
-  # 		addresses_attributes.each do |address_attributes|
-  # 			self.addresses.build(address_attributes)
-  # 		end
-  # 	end
-
 end
