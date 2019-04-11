aa5d0015c6de9b38f7388d96dfc4f04cc21fa54c
github username <constance.hyman@mchsi.com>
2019-04-10 19:59:22 -0700

    Done.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -17,6 +17,13 @@ class PeopleController < ApplicationController
   private
 
   def person_params
-    params.require(:person).permit(:name)
+    params.require(:person).permit(:name, addresses_attributes: [
+      :street_address_1,
+      :street_address_2,
+      :city,
+      :state,
+      :zipcode,
+      :address_type
+    ])
   end
 end
